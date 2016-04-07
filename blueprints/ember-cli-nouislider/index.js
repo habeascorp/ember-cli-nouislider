6f9ccdbf7faae4da9aa98eb0a36607e6130d67a3
Sheldon Baker <sheldonnbbaker@gmail.com>
2016-04-07 12:28:18 -0700

    Use master for noUiSlider

--- blueprints/ember-cli-nouislider/index.js
+++ blueprints/ember-cli-nouislider/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   afterInstall: function(options) {
-    return this.addBowerPackageToProject('nouislider', 'git://github.com/habeascorp/noUiSlider'); // is a promise
+    return this.addBowerPackageToProject('nouislider', 'git://github.com/habeascorp/noUiSlider#master'); // is a promise
   }
 };
